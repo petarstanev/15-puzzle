99a9a8525c8b0446a2cf5db15999eb7928baaa74
petarstanev <petarstanev@outlook.com>
2014-08-27 17:18:54 +0100

    First commit

---
+++ src/mainpackage/Square.java
@@ -0,0 +1,19 @@
+package mainpackage;
+/* 1, 2, 3, X
+ */
+public class Square {
+	private int number; //if number = 0 it is empty
+	
+	
+	
+	Square(int number){
+		this.number= number;
+	}
+	
+	int getNumber(){
+		return number;
+	}
+	
+	
+	
+}
